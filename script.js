acadad88633c208836381a6a3884396744e94dc2
Anzela Rizakova <73994163+anzelari@users.noreply.github.com>
2021-02-16 10:23:46 +0000

    Add data

---
+++ script.js
@@ -0,0 +1,29 @@
+const planets = ['Moon', 'Sun', 'Mercury', 'Venus', 'Mars'];
+
+const elements = ['Water', 'Earth', 'Fire', 'Air'];
+
+const activities = {
+   'Moon and Water': "mediate",
+   'Sun and Water': "express your emotions through art",
+   'Mercury and Water': "talk about your feeleing",
+   'Venus and Water': "fall in love",
+   'Mars and Water': "go for swimming",
+
+   'Moon and Earth': "do yoga",
+   'Sun and Earth': "take a massage",
+   'Mercury and Earth': "play with modeling clay ",
+   'Venus and Earth': "eat something delicious ",
+   'Mars and Earth': "cook a dinner for your family",
+
+   'Moon and Fire': "dance",
+   'Sun and Fire': "add your picture to Instagram",
+   'Mercury and Fire': "be straightforward and tell the truth",
+   'Venus and Fire': "show your love, be expressive and creative",
+   'Mars and Fire': "start something new",
+
+   'Moon and Air': "visit Facebook",
+   'Sun and Air': "do volunteering or donate",
+   'Mercury and Air': "meet your friends ",
+   'Venus and Air': "do something silly together with your partner",
+   'Mars and Air': "do coding"
+};
